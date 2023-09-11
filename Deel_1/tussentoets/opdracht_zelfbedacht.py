f306a85181b23f93ee475b2fb66c8939eb5b22d4
Tiesjeeee <143695533+Tiesjeeee@users.noreply.github.com>
2023-09-11 15:35:10 +0200

    tussentoets

--- Deel_1/tussentoets/opdracht_zelfbedacht.py
+++ Deel_1/tussentoets/opdracht_zelfbedacht.py
@@ -1,14 +1,14 @@
 from time import sleep
 
-aantal_meldingen = 1
+aantal_meldingen = 10
 
 aantal = int(input('Hoeveel wilt u kopen?'))
 prijs = 1000000
 
-for _ in range(aantal_meldingen):
+for _ in range(aantal_meldingen): 
     print("Een moment geduld a.u.b., de de website wil niet werken.")
     sleep(1)
 print('Je ziet me niet lol')
 totaal = prijs * aantal
 
-print(f'Het scherpste prijs voor het product {aantal}  is: Eur {totaal}')
+print(f'Het scherpste prijs voor het product met hetaantal {aantal}  is: Eur {totaal}')
